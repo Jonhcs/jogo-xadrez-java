2d53a94939abb3a2815285ca6d5c79299c054c28
Jhonatan Carvalho <contatojhonatancarvalho@gmail.com>
2020-07-15 14:03:38 -0300

    add tabuleiro e classes

---
+++ src/tabuleiro/Peca.java
@@ -0,0 +1,19 @@
+package tabuleiro;
+
+public class Peca {
+	protected Posicao posicao;
+	private Tabuleiro tabuleiro;
+	
+	
+	public Peca(Tabuleiro tabuleiro) {
+		super();
+		this.tabuleiro = tabuleiro;
+	}
+
+
+	protected Tabuleiro getTabuleiro() {
+		return tabuleiro;
+	}
+
+	
+}
